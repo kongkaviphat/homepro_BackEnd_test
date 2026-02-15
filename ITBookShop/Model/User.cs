a9965f1d30894bc4171774b44410f2915bf4afa8
KK_kaviphat <kongkaviphats@gmail.com>
2026-02-15 11:08:24 +0700

    change Value

--- ITBookShop/Model/User.cs
+++ ITBookShop/Model/User.cs
@@ -2,7 +2,7 @@ namespace ItBookShop.Models;
 
 public class User
 {
-    public int userId { get; set; }
+    public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public string Fullname { get; set; } = string.Empty;
--- ITBookShop/Model/ฺBook.cs
+++ ITBookShop/Model/ฺBook.cs
@@ -2,7 +2,8 @@ namespace ItBookShop.Models;
 
 public class Book
 {
-    public int UserId { get; set; }
+    public int Id { get; set; }
+
     public string Isbn13 { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Subtitle { get; set; } = string.Empty;
